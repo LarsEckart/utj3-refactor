68881cc6c6b34cf95ed57d61e8d7aae02229550e
Jeff L <jeff@langrsoft.com>
2024-02-29 20:23:40 -0700

    cleanup highlight and format

--- src/main/java/iloveyouboss/Criterion.java
+++ src/main/java/iloveyouboss/Criterion.java
@@ -6,9 +6,8 @@ import static iloveyouboss.Weight.IRRELEVANT;
 public record Criterion(Answer answer, Weight weight) {
     // START_HIGHLIGHT
     boolean isMatch(Answer answer) {
-        return weight() == IRRELEVANT ||
-            answer.match(answer());
-    }
     // END_HIGHLIGHT
+        return weight() == IRRELEVANT || answer.match(answer());
+    }
 }
 // END:criterion
