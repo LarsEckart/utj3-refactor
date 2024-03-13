146a7869b160cb4c063625ac332bdf7c1f9da2df
Jeff L <jeff@langrsoft.com>
2024-03-12 19:37:43 -0600

    refactored

--- src/main/java/iloveyouboss/MatchSet.java
+++ src/main/java/iloveyouboss/MatchSet.java
@@ -3,31 +3,22 @@ package iloveyouboss;
 import java.util.Map;
 import static iloveyouboss.Weight.REQUIRED;
 
-// START:class
 public class MatchSet {
     private final Criteria criteria;
     private final Map<String, Answer> answers;
-    private int score;
 
     public MatchSet(Criteria criteria, Map<String, Answer> answers) {
         this.criteria = criteria;
         this.answers = answers;
-        calculateScore();
     }
 
-    private void calculateScore() {
-        score = criteria.stream()
-            .filter(criterion ->
-                criterion.isMatch(profileAnswerMatching(criterion)))
-            .mapToInt(criterion -> criterion.weight().value())
-            .sum();
-    }
-
-    public boolean isMatchFor() {
+    // START:matches
+    public boolean matches() {
         if (anyRequiredCriteriaNotMet())
             return false;
         return anyMatches();
     }
+    // END:matches
 
     private boolean anyMatches() {
         return criteria.stream()
@@ -46,8 +37,13 @@ public class MatchSet {
         return answers.get(criterion.questionText());
     }
 
+    // START:score
     public int score() {
-        return score;
+        return criteria.stream()
+            .filter(criterion ->
+                criterion.isMatch(profileAnswerMatching(criterion)))
+            .mapToInt(criterion -> criterion.weight().value())
+            .sum();
     }
+    // END:score
 }
-// END:class
