848cc05b0e7e0e381460a38f4ebf95f33284cd89
Jeff L <jeff@langrsoft.com>
2024-04-27 13:27:07 -0600

    cleanup tests

--- src/test/java/iloveyouboss/AProfilePerformance.java
+++ src/test/java/iloveyouboss/AProfilePerformance.java
@@ -15,25 +15,27 @@ import static java.util.stream.IntStream.range;
 
 class AProfilePerformance {
     int questionCount = 20;
-    Profile profile;
-    List<Question> questions = new ArrayList<>();
-    List<Answer> answers = new ArrayList<>();
+    List<Question> questions;
+    Answer[] answers;
     Criteria criteria;
     Random random = new Random();
 
     @BeforeEach
     void create() {
         createSomeQuestionsAndAnswers(questionCount);
-        createCriteria();
+        criteria = new Criteria(createCriteria());
     }
 
     void createSomeQuestionsAndAnswers(int number) {
         String[] noYes = {NO.toString(), YES.toString()};
+        questions = new ArrayList<>();
+        List<Answer> answerList = new ArrayList<>();
         range(0, number).forEach(i -> {
             var question = new Question("" + i, noYes, i);
             questions.add(question);
-            answers.add(new Answer(question, randomYesNoAnswer()));
+            answerList.add(new Answer(question, randomYesNoAnswer()));
         });
+        answers = answerList.toArray(new Answer[0]);
     }
 
     int numberOfWeights = Weight.values().length;
@@ -46,27 +48,26 @@ class AProfilePerformance {
         return random.nextInt() % 2 == 0 ? NO : YES;
     }
 
-    void createCriteria() {
-        var items = range(0, questionCount).mapToObj(i -> {
+    List<Criterion> createCriteria() {
+        return range(0, questionCount).mapToObj(i -> {
             var question = questions.get(i);
             var answer = new Answer(question, randomYesNoAnswer());
             return new Criterion(answer, randomWeight());
         }).toList();
-        criteria = new Criteria(items);
     }
 
-    // START_HIGHLIGHT
     @Test
     void executionTime() {
+        // START_HIGHLIGHT
         var iterations = 1_000_000;
         var elapsedMs = time(iterations, i -> {
-            profile = new Profile("");
-            profile.add(answers.toArray(new Answer[0]));
+            var profile = new Profile("");
+            profile.add(answers);
             profile.matches(criteria);
         });
         System.out.println(elapsedMs);
+        // END_HIGHLIGHT
     }
-    // END_HIGHLIGHT
 
     long time(int times, Consumer<Integer> func) {
         var start = System.nanoTime();
