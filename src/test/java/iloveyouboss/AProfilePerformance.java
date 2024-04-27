52a6a4cb4d9e8de1db1d3874977a179720f6e514
Jeff L <jeff@langrsoft.com>
2024-04-27 13:44:58 -0600

    big refactor

--- src/test/java/iloveyouboss/AProfilePerformance.java
+++ src/test/java/iloveyouboss/AProfilePerformance.java
@@ -1,10 +1,8 @@
 package iloveyouboss;
 
 // START:perf_test
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -15,49 +13,14 @@ import static java.util.stream.IntStream.range;
 
 class AProfilePerformance {
     int questionCount = 20;
-    List<Question> questions;
-    Answer[] answers;
-    Criteria criteria;
     Random random = new Random();
-
-    @BeforeEach
-    void create() {
-        createSomeQuestionsAndAnswers(questionCount);
-        criteria = new Criteria(createCriteria());
-    }
-
-    void createSomeQuestionsAndAnswers(int number) {
-        String[] noYes = {NO.toString(), YES.toString()};
-        questions = new ArrayList<>();
-        List<Answer> answerList = new ArrayList<>();
-        range(0, number).forEach(i -> {
-            var question = new Question("" + i, noYes, i);
-            questions.add(question);
-            answerList.add(new Answer(question, randomYesNoAnswer()));
-        });
-        answers = answerList.toArray(new Answer[0]);
-    }
-
-    int numberOfWeights = Weight.values().length;
-
-    Weight randomWeight() {
-        return Weight.values()[random.nextInt(numberOfWeights)];
-    }
-
-    YesNo randomYesNoAnswer() {
-        return random.nextInt() % 2 == 0 ? NO : YES;
-    }
-
-    List<Criterion> createCriteria() {
-        return range(0, questionCount).mapToObj(i -> {
-            var question = questions.get(i);
-            var answer = new Answer(question, randomYesNoAnswer());
-            return new Criterion(answer, randomWeight());
-        }).toList();
-    }
 
     @Test
     void executionTime() {
+        var questions = createQuestions();
+        var answers = createAnswers(questions);
+        var criteria = new Criteria(createCriteria(questions));
+
         // START_HIGHLIGHT
         var iterations = 1_000_000;
         var elapsedMs = time(iterations, i -> {
@@ -73,6 +36,36 @@ class AProfilePerformance {
         var start = System.nanoTime();
         range(0, times).forEach(i -> func.accept(i + 1));
         return (System.nanoTime() - start) / 1_000_000;
+    }
+
+    int numberOfWeights = Weight.values().length;
+
+    Weight randomWeight() {
+        return Weight.values()[random.nextInt(numberOfWeights)];
+    }
+
+    YesNo randomYesNoAnswer() {
+        return random.nextInt() % 2 == 0 ? NO : YES;
+    }
+
+    Answer[] createAnswers(List<Question> questions) {
+        return range(0, questionCount)
+            .mapToObj(i -> new Answer(questions.get(i), randomYesNoAnswer()))
+            .toArray(Answer[]::new);
+    }
+
+    List<Question> createQuestions() {
+        String[] noYes = {NO.toString(), YES.toString()};
+        return range(0, questionCount)
+            .mapToObj(i -> new Question("" + i, noYes, i))
+            .toList();
+    }
+
+    List<Criterion> createCriteria(List<Question> questions) {
+        return range(0, questionCount)
+            .mapToObj(i -> new Criterion(
+                new Answer(questions.get(i), randomYesNoAnswer()), randomWeight()))
+            .toList();
     }
 }
 // END:perf_test
