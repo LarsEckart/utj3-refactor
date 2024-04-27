2489eaf8e64185ceede8b4d558693a25f80bf4e0
Jeff L <jeff@langrsoft.com>
2024-04-27 15:25:28 -0600

    stream focus

--- src/main/java/iloveyouboss/Criteria.java
+++ src/main/java/iloveyouboss/Criteria.java
@@ -1,20 +1,24 @@
 package iloveyouboss;
 
-// START:Criteria
 import java.util.*;
 import java.util.stream.Stream;
 import static java.util.Arrays.asList;
 
-public record Criteria(List<Criterion> criteria) {
+public record Criteria(List<Criterion> criteria)
+    implements Iterable<Criterion> {
     public Criteria(Criterion... criterion) {
         this(asList(criterion));
     }
 
-    // START_HIGHLIGHT
+    // START:stream
     public Stream<Criterion> stream() {
         return criteria.stream();
     }
-    // END_HIGHLIGHT
+    // END:stream
+
+    @Override
+    public Iterator<Criterion> iterator() {
+        return criteria.iterator();
+    }
 }
 // END:Criteria
-
