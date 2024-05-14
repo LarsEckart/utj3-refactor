e78779ba2fb6be26dbcaedfebb453cb548dcf4ce
Jeff L <jeff@langrsoft.com>
2024-05-14 09:59:01 -0600

    score

--- src/main/java/iloveyouboss/Profile.java
+++ src/main/java/iloveyouboss/Profile.java
@@ -33,10 +33,8 @@ public class Profile {
                 score += criterion.weight().value();
             }
         }
-        if (kill) {
-            score = 0;
+        if (kill)
             return false;
-        }
 
         return anyMatches(criteria);
     }
