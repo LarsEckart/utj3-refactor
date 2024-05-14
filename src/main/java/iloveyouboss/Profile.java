45eb956af14f592069330ce2344e38aac2ae210e
Jeff L <jeff@langrsoft.com>
2024-05-14 09:57:41 -0600

    score

--- src/main/java/iloveyouboss/Profile.java
+++ src/main/java/iloveyouboss/Profile.java
@@ -54,10 +54,8 @@ public class Profile {
         // ...
         // START:matches
         // END:mouthful
-        if (kill) {
-            score = 0;
+        if (kill)
             return false;
-        }
 
         return anyMatches;
         // START:mouthful
